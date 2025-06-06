09bf897cb04243c966e69d43314bd923dee89bcf
arthur.patsanovskiy <arthur.patsanovskiy@gmail.com>
2025-06-06 17:53:25 +0300

    Added the migration for task indexes.

---
+++ migrations/Version20250606114008.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250606114008 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE INDEX status_idx ON task(status)
+        SQL);
+		$this->addSql(<<<'SQL'
+			CREATE INDEX deadline_idx ON task(deadline)
+		SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP INDEX status_idx
+        SQL);
+		$this->addSql(<<<'SQL'
+            DROP INDEX deadline_idx
+        SQL);
+    }
+}
